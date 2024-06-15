7e8d2c36be30a8fc39af1c0b69608f9738f13d62
muthukumarpl <77067221+muthukumarpl@users.noreply.github.com>
2024-06-15 10:11:26 +0530

    user input in python - input()

---
+++ userinput.py
@@ -0,0 +1,24 @@
+# input()
+# int(input())
+# float(input))
+# () - paranthesis , {} - curly braces , [] - square brackets
+
+
+"""user_name = input("enter your user name")
+print("your user name is",user_name)
+print(type(user_name))
+
+ratings = int(input("enter your ratings between 1-10"))
+print(ratings)
+print(type(ratings))
+
+percent = float(input("enter your percentage:"))
+print(percent)
+print(type(percent))"""
+
+num1 = 10
+num2 = 2
+print(num1 // num2) 
+print(num1 / num2 )
+print (num1 % num2)
+
